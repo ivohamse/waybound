be2bf4d011554eb27541b61d83ae77509480beed
Ivo Hamse <65652850+ivohamse@users.noreply.github.com>
2026-09-08 10:33:30 +0200

    refactor: define explicit public package exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,33 @@
-export * from "#core";
-export * from "#types";
+export { Router, WayboundError } from "#core";
+export type {
+  CapabilityFeature,
+  CapabilityOption,
+  CapabilitySemantics,
+  FeatureCapability,
+  ProviderCapabilities,
+  ProviderType,
+  RouterConfig,
+  WayboundErrorCode,
+  WayboundErrorOptions,
+} from "#core";
+export type {
+  Coordinate,
+  ProfileType,
+  RouteOptions,
+  RouteQuery,
+  RouteResult,
+  RouteResponse,
+  Maneuver,
+  NearestOptions,
+  NearestQuery,
+  NearestResult,
+  NearestResponse,
+  MatrixOptions,
+  MatrixQuery,
+  MatrixResponse,
+  IsochroneOptions,
+  IsochroneQuery,
+  IsochroneResult,
+  IsochroneResponse,
+} from "#types";
 export type { HttpClientOptions } from "#http";
